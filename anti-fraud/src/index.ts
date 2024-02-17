7da99809e338e9bf881f4bc7c03e8c8f88ea22f0
vsantos1711 <vsantos067100@gmail.com>
2024-02-17 18:55:38 -0300

    chore: change port of anti-fraud service

--- anti-fraud/src/index.ts
+++ anti-fraud/src/index.ts
@@ -1,6 +1,6 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => "Hello Elysia").listen(3333);
 
 console.log(
   `🪼  Anti-fraud microservice is running at ${app.server?.hostname}:${app.server?.port}`
