dcafcf20da8f7fad75f9fe3fa76efafa89c2c29f
vsantos1711 <vsantos067100@gmail.com>
2024-02-18 17:28:17 -0300

    feat: update standard migrate

--- transaction/drizzle/0000_sour_morph.sql
+++ transaction/drizzle/0000_sour_morph.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS "transactions" (
-	"account_external_id_debit" varchar,
-	"account_external_id_credit" varchar,
-	"transferTypeId" integer,
-	"status" varchar,
-	"value" integer,
+	"account_external_id_debit" varchar NOT NULL,
+	"account_external_id_credit" varchar NOT NULL,
+	"transferTypeId" integer NOT NULL,
+	"status" varchar NOT NULL,
+	"value" integer NOT NULL,
 	CONSTRAINT "transactions_account_external_id_debit_unique" UNIQUE("account_external_id_debit"),
 	CONSTRAINT "transactions_account_external_id_credit_unique" UNIQUE("account_external_id_credit")
 );
