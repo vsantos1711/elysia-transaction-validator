e116a7fea8afa7acfbb4ce08054ffbe2851225c8
vsantos1711 <vsantos067100@gmail.com>
2024-02-18 01:17:20 -0300

    chore: create a consumer to verify the transaction and a producer to return the new status

--- anti-fraud/src/index.ts
+++ anti-fraud/src/index.ts
@@ -1,7 +1,30 @@
 import { Elysia } from "elysia";
+import { consumer, producer, setupConsumers, setupProducers } from "./kafka";
 
-const app = new Elysia().get("/", () => "ANTI-FRAUD MICROSERVICE").listen(3003);
+await setupConsumers();
+await setupProducers();
 
-console.log(
-  `🪼  Anti-fraud microservice is UP at ${app.server?.hostname}:${app.server?.port}`
-);
+async function send() {
+  await producer.send({
+    topic: "anti-fraud",
+    messages: [{ value: "AQUI EU RETORNO O NOVO STATUS DA TRANSACTION" }],
+  });
+}
+
+await consumer.run({
+  eachMessage: async ({ topic, partition, message }) => {
+    console.log({
+      value: message.value?.toString(),
+      text: "AQUI EU VERIFICO A TRANSACTION!",
+    });
+    await send();
+  },
+});
+
+const app = new Elysia().get("/", () => "ANTI-FRAUD SERVICE").listen(3333);
+
+console.log(`
+==================================================================================
+🪼  Anti-fraud microservice is UP at ${app.server?.hostname}:${app.server?.port}
+==================================================================================
+`);
